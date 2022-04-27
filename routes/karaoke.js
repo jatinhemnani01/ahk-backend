3537a38a5fb0b59465dee2163d02b4cf07fc1888
jatinhemnani01 <jatinhemnani01@gmail.com>
2022-04-27 23:48:57 +0530

    added function to update karaoke

--- routes/karaoke.js
+++ routes/karaoke.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/db");
 
-
 // GET ALL KARAOKE
 router.get("/all", (req, res) => {
-  db.query("select * from `ahk-db`.all_karaoke;", (err, result) => {
+  db.query("select * from all_karaoke;", (err, result) => {
     if (err) {
-      console.log(err);
       res.status(500);
       res.json({
         ok: false,
@@ -39,7 +37,6 @@ router.post("/add_karaoke", (req, res) => {
 
   db.query("insert into `ahk-db`.all_karaoke SET ?", karaoke, (err, result) => {
     if (err) {
-      console.log(err);
       res.status(500);
       res.json({
         ok: false,
@@ -56,39 +53,57 @@ router.post("/add_karaoke", (req, res) => {
 
 // ADD KARAOKE TO DB
 
-
 // DELETE KARAOKE FROM DB
 
-router.delete("/delete/:id",(req,res)=>{
-  const {id}=req.params;
+router.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
 
-  const query=`delete from all_karaoke where kid=${id}`
+  const query = `delete from all_karaoke where kid=${id}`;
 
-  db.query(query,(err,result)=>{
-    if(err){
+  db.query(query, (err, result) => {
+    if (err) {
       res.status(500);
       res.json({
-        ok:false,
-        message:"Error while deleting data from database"
-      })
+        ok: false,
+        message: "Error while deleting data from database",
+      });
     } else {
       res.status(200);
-      console.log(result);
       res.json({
-        ok:true,
-        message:"Karaoke delted from database"
-      })
+        ok: true,
+        message: "Karaoke delted from database",
+      });
     }
-  })
-
-})
-
-
+  });
+});
 
 // DELETE KARAOKE FROM DB
 
+// UPDATE KARAOKE
+router.patch("/update", (req, res) => {
+  const { kid, name, artist, album, year, album_cover_art, duet } = req.body;
 
+  const query =
+    "update all_karaoke set name=?, artist=?,album=?,year=?,album_cover_art=?,duet=? where kid=?";
+  const data = [name, artist, album, year, album_cover_art, duet, kid];
 
+  db.query(query, data, (err, result) => {
+    if (err) {
+      res.status(400);
+      res.json({
+        ok: false,
+        message: "Error while updating data in database",
+      });
+    } else {
+      res.status(200);
+      res.json({
+        ok: true,
+        message: "data updated in the database",
+      });
+    }
+  });
+});
 
+// UPDATE KARAOKE
 
 module.exports = router;

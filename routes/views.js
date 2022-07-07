9610a2003279b7a1d444fa75ccd6b79e4858851d
jatinhemnani01 <jatinhemnani01@gmail.com>
2022-07-07 15:32:59 +0530

    feat: views api done

--- routes/views.js
+++ routes/views.js
@@ -2,9 +2,35 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/db");
 
+// function to convert views to string with commas
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 router.post("/count", (req, res) => {
   const { kid } = req.query;
-  res.send(kid);
+  const query = `SELECT views from all_karaoke ak WHERE kid=${kid};`;
+
+  db.query(query, (err, result) => {
+    if (err || result.length == 0) {
+      console.log(err);
+      res.json({ message: "No data!" });
+    } else {
+      const newView = result[0].views + 1;
+      db.query(
+        `UPDATE all_karaoke SET views=? WHERE kid=${kid};`,
+        [newView],
+        (err, result) => {
+          if (err) {
+            res.status(400);
+            res.json({ message: "Error on updating views!" });
+          } else {
+            res.status(200).json({ views: numberWithCommas(newView) });
+          }
+        }
+      );
+    }
+  });
 });
 
 module.exports = router;
